ef26e6518ccd98cb72df66edc983804911f1a980
pgm-brentvansteertegem <brent.vansteertegem@student.arteveldehs.be>
2021-12-29 15:45:41 +0100

    Almost finished styling

--- static/js/main.js
+++ static/js/main.js
@@ -15,8 +15,15 @@
       this.$weather = document.querySelector('#weather');  
       this.$githubUsers = document.querySelector('#github_users');
       this.$userAvatar = document.querySelector('#user_avatar');
-      this.$pgmUser = document.querySelector('#pgm_user')
+      this.$pgmUser = document.querySelector('#pgm_user');
+      this.$dashboardUsername = document.querySelector('#dashboard_username');
       this.$quote = document.querySelector('#quote');
+      this.$labelIsLecturer = document.querySelector('#label_is_lecturer');
+      this.$isLecturer = document.querySelector('#is_lecturer');
+      this.$birthday = document.querySelector('#birthday');
+      this.$linkedInUsername = document.querySelector('#linkedIn_username');
+      this.$userType = document.querySelector('#user_type');
+      this.$linkPage = document.querySelector('#link_page');
       this.$repositories = document.querySelector('#repositories');
       this.$followers = document.querySelector('#followers');
       this.$pgmTeam = document.querySelector('#pgm_team');  
@@ -73,23 +80,52 @@
     });
     },
     loadDashboard (data, internalData) {
+      this.$repositories.innerHTML = "<p>Loading..</p>";
+      this.$followers.innerHTML = "<p>Loading..</p>";
+      let username;
       if (internalData) {   
         if (this.$pgmUser.classList.contains('hidden')) {
           this.$pgmUser.classList.remove('hidden');   
         }             
         username = data.portfolio.github_gebruikersnaam;
-        this.$userAvatar.src = data.thumbnail;
+        this.$userAvatar.style.backgroundImage = `url("${data.thumbnail}")`;
+        this.$dashboardUsername.innerHTML = username;
         if (data.lijfspreuk != ""){
           this.$quote.innerHTML = data.lijfspreuk;
         } else {
           this.$quote.innerHTML = 'Deze gebruiker heeft geen lijfspreuk opgegeven.'
         }
+        this.$labelIsLecturer.innerHTML = username;
+        if (data.is_docent) {
+          this.$isLecturer.innerHTML = `${data.voornaam} ${data.familienaam} is een docent binnen het pgm-team.`
+        } else {
+          this.$isLecturer.innerHTML = `${data.voornaam} ${data.familienaam} is een student binnen het pgm-team.`
+        }
+        this.$birthday.innerHTML = `${Math.floor((Math.floor(new Date().getTime()/1000) - data.geboortedatum)/3600/24/365)} jaar`;
+        if (data.portfolio.linkedin_gebruikersnaam != ""){
+          this.$linkedInUsername.innerHTML = data.portfolio.linkedin_gebruikersnaam;
+        } else {
+          this.$linkedInUsername.innerHTML = 'Deze gebruiker heeft geen LinkedIn gebruikersnaam opgegeven.'
+        }
+        getUser(username)
+        .then((externalData) => {
+          this.$userType.innerHTML = externalData.type;
+          this.$linkPage.innerHTML = `Klik hier om naar de pagina van <span>${username}</span> te gaan.`;
+          this.$linkPage.href = externalData.html_url;
+        })
+        .catch((error) => {
+          console.log(error)
+        });
       } else {
         if (!this.$pgmUser.classList.contains('hidden')) {
           this.$pgmUser.classList.add('hidden');   
         }         
         username = data.login;
-        this.$userAvatar.src = data.avatar_url;
+        this.$userAvatar.style.backgroundImage = `url("${data.avatar_url}")`;
+        this.$dashboardUsername.innerHTML = username;
+        this.$userType.innerHTML = data.type;
+        this.$linkPage.innerHTML = `Klik hier om naar de pagina van <span>${username}</span> te gaan.`;
+          this.$linkPage.href = data.html_url;
       }
       getRepositories(username)
         .then((repositories) => {
@@ -105,12 +141,15 @@
       `;
             }).join('');
             this.$followers.innerHTML = followers.map((follower) => {
+              console.log(follower);
               return `
                 <li>
-                  <div>
-                    <img src="${follower.avatar_url}" loading="lazy" alt="avatar" />
-                    <p class="username">${follower.login}</p>
-                  </div>
+                <a href="${follower.html_url}">
+                    <div>
+                      <img src="${follower.avatar_url}" loading="lazy" alt="avatar" />
+                      <p class="username">${follower.login}</p>
+                    </div>
+                  </a>
                 </li>          
       `;
             }).join('');
@@ -151,10 +190,12 @@
               return `
                 <li>
                   <div class="pgm_list_item">
-                    <img src="${user.thumbnail}" loading="lazy" alt="Avatar" />
-                    <p class="username">${user.portfolio.github_gebruikersnaam}</p>
-                  </div>
-                  <p>${user.voornaam} ${user.familienaam}</p>
+                    <div>
+                      <img src="${user.thumbnail}" loading="lazy" alt="Avatar" />
+                      <p class="username">${user.portfolio.github_gebruikersnaam}</p>
+                    </div>
+                    <p>${user.voornaam} ${user.familienaam}</p>
+                  <div>
                 </li>          
 `;
             }).join('');
